1fa265e4781a781e9434ac9367d41bd535cc9a0b
wisdomwest <harmonykoome@gmail.com>
2023-11-01 03:21:20 -0700

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_size - get size of binary tree
+  * @tree: pointer to root node
+  * Return: size or 0
+  */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
