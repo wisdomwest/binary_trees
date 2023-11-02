6ab3bf95d9fa903566e54b3eed3c03c3720f01aa
wisdomwest <harmonykoome@gmail.com>
2023-11-02 01:12:47 -0700

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+  * binary_tree_is_leaf - checks if node is leaf
+  * @node: pointer to node
+  * Return: 0 or 1
+  */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
+
+/**
   * binary_tree_leaves - number of leaves
   * @tree: pointer to root node
   * Return: number or 0
