62a9b0e722de877ae6f7f994ef6c45ea190179b1
wisdomwest <harmonykoome@gmail.com>
2023-11-02 12:06:41 -0700

    compile issue

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -26,5 +26,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (NULL);
-	return (binary_tree_sibling(node->parent))
+	return (binary_tree_sibling(node->parent));
 }
