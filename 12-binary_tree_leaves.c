92d5bbb01e5515056ee2673a8f8c35992938d96a
wisdomwest <harmonykoome@gmail.com>
2023-11-02 01:06:09 -0700

    task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_leaves - number of leaves
+  * @tree: pointer to root node
+  * Return: number or 0
+  */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_is_leaf(tree) +
+			binary_tree_leaves(tree->right) +
+			binary_tree_leaves(tree->left));
+	}
+	else
+		return (0);
